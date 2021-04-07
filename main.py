ab095fa6b07b9d9491c22fe657a70efbe65caa9f
atrox3d <atrox3d@gmail.com>
2021-04-08 00:05:54 +0200

    my setup

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for
+)
+import util.network
+import util.params
+from util.logging import(
+    get_root_logger,
+    log_decorator
+)
+import logging
+logger = logging.getLogger(__name__)
+get_root_logger()
 
 app = Flask(__name__)
 
@@ -6,15 +21,17 @@ all_books = []
 
 
 @app.route('/')
+@log_decorator
 def home():
     pass
 
 
 @app.route("/add")
+@log_decorator
 def add():
     pass
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host=util.network.get_ipaddress())
 
