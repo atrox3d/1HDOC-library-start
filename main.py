c198cdb4404dadeb15391dafe959c69d36a3c03b
atrox3d <atrox3d@gmail.com>
2021-04-08 06:55:09 +0200

    add books

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from flask import (
 )
 import util.network
 import util.params
-from util.logging import(
+from util.logging import (
     get_root_logger,
     log_decorator
 )
 import logging
+
 logger = logging.getLogger(__name__)
 get_root_logger()
 
@@ -23,15 +24,24 @@ all_books = []
 @app.route('/')
 @log_decorator
 def home():
-    pass
+    return render_template('index.html')
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 @log_decorator
 def add():
-    pass
+    logger.info(request.form)
+    logger.info(request.method)
+    if request.method == "POST":
+        title = request.form['title']
+        author = request.form['author']
+        rating = request.form['rating']
+        logger.info(f"{title}, {author}, {rating}")
+        all_books.append(dict(title=title, author=author, rating=rating))
+        for book in all_books:
+            logger.info(str(book))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True, host=util.network.get_ipaddress())
-
